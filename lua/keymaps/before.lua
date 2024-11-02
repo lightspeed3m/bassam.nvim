c0ab0ed08090a188c1ff3af9170046b7c3a69812
Super User <root@bassam.io>
2024-11-03 01:02:52 +0300

    Modifications

--- lua/keymaps/before.lua
+++ lua/keymaps/before.lua
@@ -30,3 +30,9 @@ vim.keymap.set("n", "<leader>Y", "\"+Y")
 -- Delete into void register
 vim.keymap.set("n", "<leader>d", "\"_d")
 vim.keymap.set("v", "<leader>d", "\"_d")
+
+-- Navigate vim panes better
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
